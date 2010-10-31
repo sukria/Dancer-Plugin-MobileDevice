aea38c692504162977f319aeeb646d1d6c57a796
Alexis Sukrieh <sukria@sukria.net>
2010-11-01 00:35:33 +0100

    working version

--- lib/Dancer/Plugin/MobileDevice.pm
+++ lib/Dancer/Plugin/MobileDevice.pm
@@ -1,52 +1,58 @@
 package Dancer::Plugin::MobileDevice;
 
-use warnings;
 use strict;
+use warnings;
+our $VERSION = '0.01';
+    
+use Dancer ':syntax';
+use Dancer::Plugin;
 
+register 'is_mobile_device' => sub {
+    return request->user_agent =~
+        /(iPhone|Android|BlackBerry|Mobile|Palm)/
+      ? 1 : 0;
+};
+
+before sub {
+    var orig_layout => setting('layout');
+
+    if (is_mobile_device()) {
+        setting layout => 'mobile';
+    }
+};
+
+after sub {
+    my $orig_layout = vars->{'orig_layout'};
+    setting layout => $orig_layout;
+};
+
+before_template sub {
+    my $tokens = shift;
+    $tokens->{'is_mobile_device'} = is_mobile_device();
+};
+
+register_plugin;
+
+1;
+__END__
 =head1 NAME
 
-Dancer::Plugin::MobileDevice - The great new Dancer::Plugin::MobileDevice!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+Dancer::Plugin::MobileDevice - make a dancr app mobile-aware
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
+    package MyWebApp;
+    use Dancer;
     use Dancer::Plugin::MobileDevice;
 
-    my $foo = Dancer::Plugin::MobileDevice->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+    get '/' => sub {
+        if (is_mobile_device) {
+            # do something for mobile
+        }
+        else {
+            # do something for regular agents
+        }
+    };
 
 =head1 AUTHOR
 
@@ -54,12 +60,8 @@ Alexis Sukrieh, C<< <sukria at sukria.net> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-dancer-plugin-mobiledevice at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer-Plugin-MobileDevice>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to
+L<http://github.com/sukria/Dancer-Plugin-MobileDevice/issues>
 
 =head1 SUPPORT
 
@@ -93,6 +95,8 @@ L<http://search.cpan.org/dist/Dancer-Plugin-MobileDevice/>
 
 =head1 ACKNOWLEDGEMENTS
 
+This plugin was initially written for an artilce of the Dancer advent calendar
+2010.
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -104,7 +108,4 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
-
 =cut
-
-1; # End of Dancer::Plugin::MobileDevice

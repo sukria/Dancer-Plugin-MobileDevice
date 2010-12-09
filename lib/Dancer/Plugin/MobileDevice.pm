fccaab2ed78cddb9a5f8b16fb700aa291f7ae3b4
David Precious <davidp@preshweb.co.uk>
2010-12-09 18:59:51 +0000

    Make mobile layout configurable.
    Only change the layout if configured to do so, and make the layout name used for
    mobile devices configurable.

--- lib/Dancer/Plugin/MobileDevice.pm
+++ lib/Dancer/Plugin/MobileDevice.pm
@@ -13,11 +13,17 @@ register 'is_mobile_device' => sub {
       ? 1 : 0;
 };
 
-before sub {
-    var orig_layout => setting('layout');
 
-    if (is_mobile_device()) {
-        setting layout => 'mobile';
+before sub {    
+    # If we don't have a mobile layout declared, do nothing.
+    my $settings = plugin_setting || {};
+    if (my $mobile_layout = $settings->{mobile_layout}) {
+        # OK, remember the original layout setting (so we can restore it
+        # after the request), and override it with the specified mobile layout.
+        if (is_mobile_device()) {
+            var orig_layout => setting('layout');
+            setting layout => $mobile_layout;
+        }
     }
 };
 
@@ -59,6 +65,30 @@ Dancer::Plugin::MobileDevice - make a Dancer app mobile-aware
 A plugin for L<Dancer>-powered webapps to easily detect mobile clients and offer
 a simplified layout, and/or act in different ways.
 
+The plugin offers a C<is_mobile_device> keyword, which returns true if the
+device is recognised as a mobile device.
+
+It can also automatically change the layout used to render views for mobile
+clients.
+
+=head1 Custom layout for mobile devices
+
+This plugin can use a custom layout for recognised mobile devices, allowing you
+to present a simplified page template for mobile devices.  To enable this, use
+the C<mobile_layout> setting for this plugin - for instance, add the following
+to your config file:
+
+  plugins:
+    mobiledevice:
+      mobile_layout: 'mobile'
+
+This means that, when C<template> is called to render a view, if the client is
+recognised as a mobile device, the layout named C<mobile> will be used, rather
+than whatever the current C<layout> setting is.
+
+You can of course still override this layout by supplying a layout option to the
+C<template> call in the usual way (see the L<Dancer> documentation for how to do
+this).
 
 =head1 AUTHOR
 
